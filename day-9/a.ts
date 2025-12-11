84ba7911ae4fe9e4fa1af9d86ad3c1129b33bd52
Illia Abashkin <abashkinis@gmail.com>
2025-12-11 17:06:23 +0200

    9-a

--- day-9/a.ts
+++ day-9/a.ts
@@ -2,8 +2,25 @@ import { runSolution } from '../utils.ts';
 
 /** provide your solution as the return of this function */
 export async function day9a(data: string[]) {
-  console.log(data);
-  return 0;
+  let maxSquare = 0;
+  const numberOfTiles = data.length;
+  for (let i = 0; i < numberOfTiles; i++) {
+    for (let j = i + 1; j < numberOfTiles; j++) {
+      const p1 = data[i].split(',').map(Number);
+      const p2 = data[j].split(',').map(Number);
+      const square = getRectangleSquare(p1, p2);
+      maxSquare = Math.max(maxSquare, square);
+    }
+  }
+
+  return maxSquare;
+}
+
+function getRectangleSquare(p1: number[], p2: number[]) {
+  const dx = Math.abs(p1[0] - p2[0] + 1);
+  const dy = Math.abs(p1[1] - p2[1] + 1);
+
+  return dx * dy;
 }
 
 await runSolution(day9a);
